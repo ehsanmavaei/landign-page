17d509498df5d5fa5ecf1af55a7d8c62f9da09f4
ehsan <ehsanmavaei@gmail.com>
2021-01-18 22:19:55 +0330

    finish the header add small menu and sync with the toggle menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,7 @@
 const hamburger=document.querySelector('#btnHamburger')
 const overlay=document.querySelector('#overlay')
+const samllmenu=document.querySelector('#smallmenu');
+const body=document.querySelector('body')
 hamburger.addEventListener('click',function(){
   
 
@@ -7,9 +9,16 @@ hamburger.addEventListener('click',function(){
 if(hamburger.classList.contains('open')){
     this.classList.remove('open')
     overlay.classList.remove('overlay-open');
+    samllmenu.classList.add('hasfade');
+    body.classList.remove('noscroll')
 }else{
     this.classList.add('open');
     overlay.classList.add('overlay-open');
+    samllmenu.classList.remove('hasfade');
+    body.classList.add('noscroll')
+   
+   
+  
 
 }
 })
