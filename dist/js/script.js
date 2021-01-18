45dce80b3c70f19e8254c8d1797ba2f7bd77fb40
ehsan <ehsanmavaei@gmail.com>
2021-01-18 13:14:44 +0330

    toggle hamburger menu to X and back agian on click

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,12 @@
-console.log('hello there');
+const hamburger=document.querySelector('#btnHamburger')
+hamburger.addEventListener('click',function(){
+  
+
+
+if(hamburger.classList.contains('open')){
+    this.classList.remove('open');
+}else{
+    this.classList.add('open')
+
+}
+})
