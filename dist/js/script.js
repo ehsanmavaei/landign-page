fce4234cffd6cbab78dc9405c6b5912fad16e640
ehsan <ehsanmavaei@gmail.com>
2021-01-18 17:12:13 +0330

    add the overlay and sync it with the hamburger menu

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,15 @@
 const hamburger=document.querySelector('#btnHamburger')
+const overlay=document.querySelector('#overlay')
 hamburger.addEventListener('click',function(){
   
 
 
 if(hamburger.classList.contains('open')){
-    this.classList.remove('open');
+    this.classList.remove('open')
+    overlay.classList.remove('overlay-open');
 }else{
-    this.classList.add('open')
+    this.classList.add('open');
+    overlay.classList.add('overlay-open');
 
 }
 })
